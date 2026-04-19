9297750c3a221e52d555e412b97df4425c0a31ac
Gabor Szabo <gabor@szabgab.com>
2026-04-19 20:36:39 +0300

    add day2 circle

---
+++ day02/circle.py
@@ -0,0 +1,16 @@
+import math
+
+radius = input("Enter the radius of the circle: ")
+print(radius)
+radius = int(radius)
+print(radius)
+area = math.pi * radius ** 2
+print(area)
+
+#radius = float(input("Enter the radius of the circle: "))
+#
+#area = math.pi * radius ** 2
+#circumference = 2 * math.pi * radius
+#
+#print(f"Area: {area}")
+#print(f"Circumference: {circumference}")
