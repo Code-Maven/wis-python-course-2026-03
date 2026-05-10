9d4112a035bc45c6f0a4d3e4bdce214b4bec6480
Gabor Szabo <gabor@szabgab.com>
2026-05-10 09:51:12 +0300

    first version of image loading

---
+++ day05/show_image.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from PIL import Image
+
+
+def main() -> None:
+    #print(type(__file__))
+    #print(dir(__file__))
+    #print(__file__)
+    o = Path(__file__)
+    #print(type(o))
+    #print(dir(o))
+    #print(o)
+    image_path = Path(__file__).with_name("shutterstock_432248797-1024x683.jpg")
+    image = Image.open(image_path)
+    #print(image)
+    #print(type(image))
+    #print(dir(image))
+    image.show()
+
+
+if __name__ == "__main__":
+    main()
